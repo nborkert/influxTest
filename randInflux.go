4cf67bb478308e9792adc6f1c3c614988aa11649
Neal Borkert <neal.borkert@gmail.com>
2017-07-09 17:31:59 -0400

    first commit

---
+++ randInflux.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+)
+
+func main() {
+	max, _ := strconv.Atoi(os.Args[1])
+	targetPage := os.Args[2]
+
+
+
+	for count := 0; count != max; count++ {
+		GetHTTPCode(targetPage, count)
+	}
+}
+
+func GetHTTPCode(u string, num int) {
+	t := time.Now()
+	res, err := http.Get(u)
+	t2 := time.Now()
+
+	if err != nil {
+		fmt.Printf("%v,"+err.Error()+",%v\n", num, t2.Sub(t))
+	}
+	if err == nil {
+		defer res.Body.Close()
+		fmt.Printf("%v,"+res.Status+",%v\n", num, t2.Sub(t))
+	}
+}
